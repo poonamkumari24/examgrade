29d3929138342f71a72eb8f2ba2a8244de42944a
poonamkumari24 <poonamshaw2415@gmail.com>
2026-05-01 12:58:06 +0530

    Cors and SecurityConfig
    updated.

--- examgrade/src/main/java/com/examgrade/controller/UserController.java
+++ examgrade/src/main/java/com/examgrade/controller/UserController.java
@@ -1,26 +1,23 @@
 package com.examgrade.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.examgrade.dto.RegisterRequest;
 import com.examgrade.dto.UserDTO;
 import com.examgrade.entity.User;
 import com.examgrade.service.UserService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -31,20 +28,16 @@ public class UserController {
     // REGISTER USER
 @PostMapping("/register")
 public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest request) {
-    try {
-        User user = new User();
-        user.setName(request.getName());
-        user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
-        user.setRole(request.getRole());
 
-        User savedUser = userService.registerUser(user);
+    User user = new User();
+    user.setName(request.getName());
+    user.setEmail(request.getEmail());
+    user.setPassword(request.getPassword());
+    user.setRole(request.getRole());
 
-        return ResponseEntity.ok(mapToDTO(savedUser));
+    User savedUser = userService.registerUser(user);
 
-    } catch (RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
+    return ResponseEntity.ok(mapToDTO(savedUser));
 }
     // GET ALL USERS
    @GetMapping
@@ -76,18 +69,5 @@ private UserDTO mapToDTO(User user) {
     dto.setRole(user.getRole());
     return dto;
 }
-@RestControllerAdvice
-public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationErrors(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-            errors.put(error.getField(), error.getDefaultMessage())
-        );
-
-        return errors;
-    }
-}
 }
