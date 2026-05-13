338beda556bcb589b373a11d561e6769d37a0e50
poonamkumari24 <poonamshaw2415@gmail.com>
2026-05-13 20:18:40 +0530

    Created QuizResultDTO.java
    dto

---
+++ examgrade/src/main/java/com/examgrade/dto/QuizResultDTO.java
@@ -0,0 +1,71 @@
+package com.examgrade.dto;
+
+public class QuizResultDTO {
+
+    private int totalQuestions;
+    private int correctAnswers;
+    private int incorrectAnswers;
+    private int obtainedMarks;
+    private int totalMarks;
+    private double percentage;
+    private boolean passed;
+
+    public QuizResultDTO() {
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(int totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(int correctAnswers) {
+        this.correctAnswers = correctAnswers;
+    }
+
+    public int getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(int incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+
+    public int getObtainedMarks() {
+        return obtainedMarks;
+    }
+
+    public void setObtainedMarks(int obtainedMarks) {
+        this.obtainedMarks = obtainedMarks;
+    }
+
+    public int getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(int totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+}
