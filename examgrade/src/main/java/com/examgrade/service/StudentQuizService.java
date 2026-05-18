a13a9295952c53f53dde5b5e20a190be1026b4b1
poonamkumari24 <poonamshaw2415@gmail.com>
2026-05-18 21:21:43 +0530

    Updated StudentQuizService.java
    correction

--- examgrade/src/main/java/com/examgrade/service/StudentQuizService.java
+++ examgrade/src/main/java/com/examgrade/service/StudentQuizService.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import com.examgrade.dto.QuizAttemptDTO;
 import com.examgrade.dto.QuizResponseDTO;
+import com.examgrade.dto.QuizResultDTO;
+import com.examgrade.dto.QuizSubmissionDTO;
 
 public interface StudentQuizService {
 
     List<QuizResponseDTO> getAllAvailableQuizzes();
     QuizAttemptDTO getQuizForAttempt(Long quizId);
+      QuizResultDTO submitQuiz(Long quizId, QuizSubmissionDTO submission);
 }
